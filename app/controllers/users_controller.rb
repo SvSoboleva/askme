b5eab6958f07d6011264021cb0f760e8c420ee6d
Lana <sv.soboleva@list.ru>
2017-05-08 20:35:52 +0300

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,53 @@
 class UsersController < ApplicationController
   # Это действие отзывается, когда пользователь заходит по адресу /users
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
-    # модели мы просто вызываем метод User.new, который создает модель, не
-    # записывая её в базу.
-    @users = [
-        User.new(
-            id: 1,
-            name: 'Vadim',
-            username: 'installero',
-            avatar_url: 'https://secure.gravatar.com/avatar/' \
-          '71269686e0f757ddb4f73614f43ae445?s=100'
-        ),
-        User.new(id: 2, name: 'Misha', username: 'aristofun')
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: ''
-    )
+    @questions = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
+  end
 
-    @questions = [
-        Question.new(text: 'Как дела?', created_at: Date.parse('03.05.2017')),
-        Question.new(text: 'Как погода?', created_at: Date.parse('03.05.2017')),
-        Question.new(text: 'Который час?', created_at: Date.parse('03.05.2017')),
-        Question.new(text: 'Как настроение?', created_at: Date.parse('03.05.2017'))
-    ]
+  private
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 
-    @new_question = Question.new
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
   end
 end
